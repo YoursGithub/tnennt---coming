18ba121e935a2d3403c7e2414492284c4a1743bb
YoursGithub <subhajitbhatt130@gmail.com>
2024-08-06 12:09:45 +0530

    Correction 1

--- src/pages/Deleteacc.jsx
+++ src/pages/Deleteacc.jsx
@@ -6,7 +6,7 @@ const Deleteacc = () => {
         <div className="bg-gray-100 min-h-screen flex justify-center items-start py-8 px-4 sm:px-6 lg:px-8">
         <div className="bg-white shadow-md rounded-lg max-w-3xl w-full mt-10 p-6 sm:p-8">
         <h1 className="text-3xl sm:text-4xl font-bold mb-2">
-        Instructions for deleting account
+        Instructions
         </h1>
         <p className="text-gray-600 mb-6 mt-3">Once you are totally sure that you want to delete your account, you can contact us at <span className='text-red-600'>support@tnennt.com</span> with valid reason for the deletion of your account. Once we check the email and see if the reason is valid, then we will proceed with the process of deletion.</p>
         </div>
